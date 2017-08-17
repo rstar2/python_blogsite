5d3dc276fc96c9f113b60dcadb217b5d19dc9697
Rumen Neshev <rstar2@abv.bg>
2017-08-18 00:26:51 +0300

    add Post admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+# This class will tweek how the Post will be displayed in admin (when in a list of all Posts)
+class PostAdmin(admin.ModelAdmin):
+    # the default is a to show just the Post.__str__() value
+    # but we can change it with
+    list_display = ('title', 'slug', 'status')
+
+    # this will add a search bar that will search in the specified fields
+    search_fields = ('title', 'body')
+
+    # this will populate the slug field when addding new post with
+    # the same value as in the title field
+    prepopulated_fields = {'slug': ('title',)}
+
+    ordering = ('status', 'published', 'slug')
+
+
+admin.site.register(Post, PostAdmin)
