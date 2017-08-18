907a58f575623b5b9feb33a7795279420d85be5c
Rumen Neshev <rstar2@abv.bg>
2017-08-18 23:52:19 +0300

    Add custom QuerySet Manager

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+    def from_rumen(self):
+        return self.get_queryset().filter(author__username='rumen')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,6 +30,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # if we add a custom query Manager and want to use the default
+    # Post.objects.all().filter()... one we have to add it explicitly
+    # It's goot to keep this the first registered Manager as
+    # it's used as default in multiple other places
+    objects = models.Manager()
+
+    # create a custom query Manager,
+    # thus we can use Post.status_published.all().filter()....
+    # Also we can use Post.status_published.from_rumen()
+    status_published = PublishedManager()
 
     def __str__(self):
         return self.title
