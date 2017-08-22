d3d48a5133c929a0587d144743b8a9e4e03796f9
Rumen Neshev <rstar2@abv.bg>
2017-08-22 23:03:57 +0300

    senf email 1

--- src/blogsite/settings.py
+++ src/blogsite/settings.py
@@ -134,3 +134,9 @@ STATICFILES_DIRS = (
 # in this folder will be collected all static files
 # when "$ python manage.py collectstatic" script is run
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'neshev.rumen@gmail.com'
+EMAIL_HOST_PASSWORD = 'cadb12'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
