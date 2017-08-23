3931e2d5b5b3eb810807d08e4678d073e6539605
rstar2 <rstar2@abv.bg>
2017-08-23 10:13:36 +0300

    add share email

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # name="blog_list" -> name of the URL to be used in HTML templates for instance
     url(r'^$', views.post_list, name="post_list"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        views.post_detail, name='post_detail')
+        views.post_detail, name='post_detail'),
+    url(r'^(?P<post_id>\d+)/share/', views.post_share, name="post_share"),
 ]
