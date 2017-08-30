5c7ebf235894281f18e944aa52735aa78fc34bca
Rumen Neshev <rstar2@abv.bg>
2017-08-30 10:18:54 +0300

    Finish tags/similar posts

--- env.sh
+++ env.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
-python3 -m venv env
-
-source env/bin/activate
-
-pip install -r requirements.txt
-
+if [ "$1" == 'install' ]
+then
+    # if this is the first time 
+    # install the Virtual environment,
+    # activate it and install needed Python packages
+    python3 -m venv env
+    source env/bin/activate
+    pip install -r requirements.txt
+else
+    # if this is normal day work - activate the environment and open VSCodess
+    source env/bin/activate
+    if [ "$1" == "code" ]
+    then
+        code .
+    fi
+fi
